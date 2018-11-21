a17c2569d3f4fdf29cbc97bf8b9f63d1617030c7
Henry T <henryt.inc@gmail.com>
2018-11-21 15:19:26 -0500

    Load port from env

--- app.js
+++ app.js
@@ -79,6 +79,6 @@ app.get('*', function(req, res) {
 
 });
 
-app.listen(3000, function () {
-    console.log('listening on *:' + 3000);
+app.listen(process.env.PORT, function () {
+    console.log('listening on *:' + process.env.POST);
 });
